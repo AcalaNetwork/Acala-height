ff7228a64cfea1c3f2fe64a7d533150c93d91e80
shenger <714275326@qq.com>
2022-05-03 21:19:25 +0800

    add apr

---
+++ apr.ts
@@ -0,0 +1,96 @@
+import { Context } from "koa";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { FixedPointNumber } from "@acala-network/sdk-core";
+import { gql, request } from 'graphql-request';
+import dayjs from "dayjs";
+
+const liquidTokenDecimals = 12;
+const stakeTokenDecimals = 12;
+
+const getTotalLiquidity = async (api: ApiPromise) => {
+  // now
+  // toBond: 21233001944
+  // legers: 80679280425263358   6840946719082802   6081837485254305
+  // == 93602085862602409
+  // 3 month age
+  // toBond: 31260778454928
+  // legers: 63904749115040723
+  // == 63936009893495651
+  const toBond = await api.query.homa.toBondPool();
+  const stakingLedgers = await api.query.homa.stakingLedgers.entries();
+  let totalInSubAccount = FixedPointNumber.ZERO;
+
+  stakingLedgers.map(item => {
+    const ledge = (item[1] as any).unwrapOrDefault();
+    console.log(ledge.bonded.unwrap().toString())
+    totalInSubAccount = totalInSubAccount.add(FixedPointNumber.fromInner(ledge.bonded.unwrap().toString(), stakeTokenDecimals));
+  })
+
+  return FixedPointNumber.fromInner(toBond.toString(), stakeTokenDecimals).add(totalInSubAccount);
+
+}
+
+// hash : 0xa78792e33353add048738c97d3a541f8d45eef5e077b12076c153a393dcf994a
+
+const getTotalStaking = async (api: ApiPromise) => {
+  // now
+  // issuance: 819202616804335856
+  // voidLiquid: 9145469841
+  //  == 819202625949805697
+  // 3 month age
+  // issuance: 585562486938164463
+  // voidLiquid: 31489984337
+  //  == 585562518428148800
+
+  const issuance = await api.query.tokens.totalIssuance({ Token: 'LKSM' });
+  const voidLiquid = await api.query.homa.totalVoidLiquid();
+  return FixedPointNumber.fromInner(issuance.toString(), liquidTokenDecimals).add(FixedPointNumber.fromInner(voidLiquid.toString(), liquidTokenDecimals));
+}
+
+export const queryBlock = () => {
+  const url = 'https://api.subquery.network/sq/AcalaNetwork/karura';
+  const monthof3age = dayjs().subtract(3, 'month').format('YYYY-MM-DDTHH:mm:ss');
+
+  return request(url, gql`
+    query {
+      blocks (filter: {timestamp: {greaterThan: "${monthof3age}"}}, orderBy: TIMESTAMP_ASC, first: 1) {
+        nodes {
+          id
+          timestamp
+          number
+        }
+      }
+    }
+  `);
+};
+
+export const calcApr = async (ctx: Context) => {
+  const wsProvider = new WsProvider('wss://karura.polkawallet.io/');
+  const api = new ApiPromise({ provider: wsProvider });
+  await api.isReady.then(() => console.log('api is ready'));
+  const totalStaking = await getTotalStaking(api);
+  const totalLiquidity = await getTotalLiquidity(api);
+
+  const blockData = await queryBlock();
+  const height = blockData.blocks.nodes[0].id;
+
+  const monthof3ageApi = await api.at(height);
+
+  const _totalStaking = await getTotalStaking(monthof3ageApi as any);
+  const _totalLiquidity = await getTotalLiquidity(monthof3ageApi as any);
+
+  const current = FixedPointNumber.fromRational(totalStaking, totalLiquidity);
+  const month3age = FixedPointNumber.fromRational(_totalStaking, _totalLiquidity);
+
+  console.log(totalStaking.toString())
+  console.log(totalLiquidity.toString())
+
+  console.log(_totalStaking.toString())
+  console.log(_totalLiquidity.toString())
+
+  ctx.body = {
+    exchangeRateCurrent: current.toNumber(),
+    exchangeRate3monthAge: month3age.toNumber(),
+    apr: current.minus(month3age).div(month3age).times(FixedPointNumber.FOUR).toNumber()
+  };
+}
--- index.ts
+++ index.ts
@@ -1,85 +1,16 @@
-import { gql, request } from 'graphql-request';
 import Koa from 'koa';
+import Router from 'koa-router';
+import { calcTime } from './blockTime'
+import { calcApr } from './apr';
 
-const app = new Koa();
+export const app = new Koa();
+const router = new Router();
 
-const karuraSubql = 'https://api.subquery.network/sq/AcalaNetwork/karura';
-const acalaSubql = 'https://api.subquery.network/sq/AcalaNetwork/acala';
+router.get('/apr', calcApr);
+router.get('/', calcTime);
 
-export const queryBlock = (network: string, height: number) => {
-  const url = network == 'KARURA' ? karuraSubql : acalaSubql;
+app.use(router.routes()).use(router.allowedMethods())
 
-  return request(url, gql`
-    query {
-      blocks(filter: {number: {equalTo: "${height}"}}) {
-        nodes {
-          id
-          timestamp
-          number
-        }
-      }
-    }
-  `);
-};
-
-export const queryLastestBlock = (network: string) => {
-  const url = network == 'KARURA' ? karuraSubql : acalaSubql;
-
-  return request(url, gql`
-    query {
-      blocks(orderBy: NUMBER_DESC, first:1)  {
-        nodes {
-          id
-          timestamp
-          number
-        }
-      }
-    }
-  `);
-};
-
-app.use(async ctx => {
-  const { network = 'Karura', recent, from, to } = ctx.request.query;
-
-  let highHeight = 0;
-  let lowHeight = 0;
-  let highTime = 0;
-  let lowTime = 0;
-  let gap = 0;
-  let data: any = {};
-
-  if (from && from != '0' && to && to != '0') {
-    highHeight = Number(from) > Number(to) ? Number(from) : Number(to);
-    lowHeight = Number(from) < Number(to) ? Number(from) : Number(to);
-
-    const high = await queryBlock((network as string).toUpperCase(), highHeight);
-    highTime = new Date(high.blocks.nodes[0].timestamp).getTime();
-    const low = await queryBlock((network as string).toUpperCase(), lowHeight);
-    lowTime = new Date(low.blocks.nodes[0].timestamp).getTime();
-    gap = highHeight - lowHeight;
-    data.from = from;
-    data.to = to;
-    data.gap = gap
-  } else {
-    const _recent = recent ? Number(recent) : 500;
-    const high = await queryLastestBlock((network as string).toUpperCase());
-    highHeight = Number(high.blocks.nodes[0].number);
-    highTime = new Date(high.blocks.nodes[0].timestamp).getTime();
-    const low = await queryBlock((network as string).toUpperCase(), highHeight - _recent);
-    lowHeight = Number(low.blocks.nodes[0].number);
-    lowTime = new Date(low.blocks.nodes[0].timestamp).getTime();
-    gap = _recent;
-    data.recent = _recent;
-    data.gap = gap
-  }
-
-  return ctx.body = {
-    avg: ((highTime - lowTime) / gap) / 1000,
-    data
-  }
-})
-
-app.listen(1020, async () => {
+app.listen(1020,() => {
   console.log('Server [karura-height] start at: ', 1020);
-
 });
