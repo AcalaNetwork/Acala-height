441a48a95df9bd5513fde444fbc31f3d9b4925e1
shenger <714275326@qq.com>
2022-05-05 10:35:08 +0800

    fix

--- apr.ts
+++ apr.ts
@@ -9,14 +9,6 @@ const liquidTokenDecimals = 12;
 const stakeTokenDecimals = 12;
 
 const getTotalStaking = async (api: ApiPromise) => {
-  // now
-  // toBond: 21233001944
-  // legers: 80679280425263358   6840946719082802   6081837485254305
-  // == 93602085862602409
-  // 3 month age
-  // toBond: 31260778454928
-  // legers: 63904749115040723
-  // == 63936009893495651
   const toBond = await api.query.homa.toBondPool();
   const stakingLedgers = await api.query.homa.stakingLedgers.entries();
   let totalInSubAccount = FixedPointNumber.ZERO;
@@ -30,17 +22,7 @@ const getTotalStaking = async (api: ApiPromise) => {
 
 }
 
-// hash : 0xa78792e33353add048738c97d3a541f8d45eef5e077b12076c153a393dcf994a
-
 const getTotalLiquidity = async (api: ApiPromise) => {
-  // now
-  // issuance: 819202616804335856
-  // voidLiquid: 9145469841
-  //  == 819202625949805697
-  // 3 month age
-  // issuance: 585562486938164463
-  // voidLiquid: 31489984337
-  //  == 585562518428148800
 
   const issuance = await api.query.tokens.totalIssuance({ Token: 'LKSM' });
   const voidLiquid = await api.query.homa.totalVoidLiquid();
@@ -85,12 +67,6 @@ export const calcApr = async (ctx: Context) => {
 
   const current = FixedPointNumber.fromRational(totalStaking, totalLiquidity);
   const month3age = FixedPointNumber.fromRational(_totalStaking, _totalLiquidity);
-
-  console.log(totalStaking.toString())
-  console.log(totalLiquidity.toString())
-
-  console.log(_totalStaking.toString())
-  console.log(_totalLiquidity.toString())
 
   const apr = current.minus(month3age).div(month3age).times(FixedPointNumber.FOUR).toNumber();
 
