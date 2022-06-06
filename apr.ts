b45eada133354ba06e625965f9b9a082b9d4f079
shenger <714275326@qq.com>
2022-06-06 11:08:00 +0800

    add acala apr

--- apr.ts
+++ apr.ts
@@ -22,24 +22,24 @@ const getTotalStaking = async (api: ApiPromise) => {
 
 }
 
-const getTotalLiquidity = async (api: ApiPromise) => {
-
-  const issuance = await api.query.tokens.totalIssuance({ Token: 'LKSM' });
+const getTotalLiquidity = async (api: ApiPromise, token: string) => {
+  const issuance = await api.query.tokens.totalIssuance({ Token: token });
   const voidLiquid = await api.query.homa.totalVoidLiquid();
   return FixedPointNumber.fromInner(issuance.toString(), liquidTokenDecimals).add(FixedPointNumber.fromInner(voidLiquid.toString(), liquidTokenDecimals));
 }
 
-export const queryBlock = () => {
-  const url = 'https://api.subquery.network/sq/AcalaNetwork/karura';
-  const monthof3age = dayjs().subtract(3, 'month').format('YYYY-MM-DDTHH:mm:ss');
+export const queryBlock = (network: string) => {
+  const url = network === 'karura' ?  'https://api.subquery.network/sq/AcalaNetwork/karura' : 'https://api.subquery.network/sq/AcalaNetwork/acala__QWNhb';
+  const monthsago = dayjs().subtract(network === 'karura' ? 3 : 1, 'month').format('YYYY-MM-DDTHH:mm:ss');
 
   return request(url, gql`
     query {
-      blocks (filter: {timestamp: {greaterThan: "${monthof3age}"}}, orderBy: TIMESTAMP_ASC, first: 1) {
+      blocks (filter: {timestamp: {greaterThan: "${monthsago}"}}, orderBy: TIMESTAMP_ASC, first: 1) {
         nodes {
           id
           timestamp
           number
+          parentHash
         }
       }
     }
@@ -47,30 +47,32 @@ export const queryBlock = () => {
 };
 
 export const calcApr = async (ctx: Context) => {
-  const redisdata = await redis.get('apr');
+  const network = ctx.request.query.network;
+  const _network = (network || 'karura') as string;
+  const redisdata = await redis.get(`apr:${_network}`);
   if (redisdata) {
     return ctx.body = redisdata;
   }
-  const wsProvider = new WsProvider('wss://karura.polkawallet.io/');
+  const wsProvider = new WsProvider(_network === 'karura' ? 'wss://karura.polkawallet.io/' : 'wss://acala.polkawallet.io/');
   const api = new ApiPromise({ provider: wsProvider });
   await api.isReady.then(() => console.log('api is ready'));
   const totalStaking = await getTotalStaking(api);
-  const totalLiquidity = await getTotalLiquidity(api);
+  const totalLiquidity = await getTotalLiquidity(api, _network === 'karura' ? 'LKSM' : 'LDOT');
 
-  const blockData = await queryBlock();
-  const height = blockData.blocks.nodes[0].id;
+  const blockData = await queryBlock(_network);
+  const height = blockData.blocks.nodes[0].parentHash;
 
-  const monthof3ageApi = await api.at(height);
+  const monthsagoApi = await api.at(height);
 
-  const _totalStaking = await getTotalStaking(monthof3ageApi as any);
-  const _totalLiquidity = await getTotalLiquidity(monthof3ageApi as any);
+  const _totalStaking = await getTotalStaking(monthsagoApi as any);
+  const _totalLiquidity = await getTotalLiquidity(monthsagoApi as any, _network === 'karura' ? 'LKSM' : 'LDOT');
 
   const current = FixedPointNumber.fromRational(totalStaking, totalLiquidity);
-  const month3age = FixedPointNumber.fromRational(_totalStaking, _totalLiquidity);
+  const monthsago = FixedPointNumber.fromRational(_totalStaking, _totalLiquidity);
 
-  const apr = current.minus(month3age).div(month3age).times(FixedPointNumber.FOUR).toNumber();
+  const apr = current.minus(monthsago).div(monthsago).times(_network === 'karura' ?  FixedPointNumber.FOUR : new FixedPointNumber(12)).toNumber();
 
-  await redis.set('apr', apr, 'EX', 60 * 60 * 24 * 3);
+  await redis.set(`apr:${_network}`, apr, 'EX', 60 * 60 * 24 * 3);
 
   return ctx.body = apr;
 }
